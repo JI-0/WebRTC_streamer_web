03ec46d7a52708db602c8be6a4cabf964e555bc8
JI-0 <janivkovic11111@gmail.com>
2023-05-08 13:06:23 +0900

    Websocket setting up functions

--- streamer.js
+++ streamer.js
@@ -10,11 +10,31 @@ const video = document.querySelector("video");
 
 //Websocket
 websocket.onopen = () => {
+    websocket.send("U\n" + username + "\n" + user_info);
 };
 
 websocket.onclose = () => {
 };
 
 websocket.onmessage = e => {
+    let parts = e.data.split("\n", 5);
+    if (parts[0] == "R") {
+        newSubscriber(parts[1]);
+    } else if (parts[0] == "A") {
+        processAnswer(parts[1], JSON.parse(parts[2]));
+    } else if (parts[0] == "C") {
+        processCandidate(parts[1], JSON.parse(parts[2]));
+    };
 };
 
+function newSubscriber(id) {
+
+};
+
+function processAnswer(id, answer) {
+
+};
+
+function processCandidate(id, candidate) {
+
+};
